4a6589a56b4b3d409e21c519efffacef7d4be7bb
FranFrioni <francescofrioni@gmail.com>
2022-02-15 04:52:10 -0300

    Probando puertos

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
-app.listen(80, () => console.log('Servidor abierto en puerto 80'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Servidor abierto en puerto ' + port));
 app.use(express.static('public'));
 app.use(express.json());
